881ddea02e9c7d047f1f0817c41d109f402a0c5d
alex <alexkautush@gmail.com>
2022-08-28 14:54:11 +0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt("Один из послдних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из послдних просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
